c40ca31860559e33e0b3090ec8087d86ce2e029b
Fred Souza <fredericoamsouza@gmail.com>
2017-08-13 18:03:37 -0300

    Controlling change of context by clicking in the toolbar icons

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -4,12 +4,25 @@ import './styles.css';
 
 export class Sidebar extends React.Component {
 
+    state = { selectedTool: null };
+
+    renderContext() {
+        switch (this.state.selectedTool) {
+            default:
+            case 'explorer':
+                return <span>Explorer</span>;
+                
+            case 'search':
+                return <span>Search</span>;
+        }
+    }
+
     render() {
         return (
             <div className="Sidebar" {...this.props}>
-                <Toolbar />
+                <Toolbar onToolSelect={(selectedTool) => this.setState({ selectedTool })} />
                 <div className="container">
-                    Sidebar
+                    {this.renderContext()}
                 </div>
             </div>
         );
--- src/components/toolbar/index.js
+++ src/components/toolbar/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Icon } from '../icon';
 import './styles.css';
 
-export const Toolbar = (props) => (
+export const Toolbar = ({ onToolSelect, ...props }) => (
     <div className="Toolbar">
-        <Icon name="file" className="icon" />
+        <Icon name="file" className="icon" onClick={() => onToolSelect('explorer')} />
+        <Icon name="file" className="icon" onClick={() => onToolSelect('search')} />
     </div>
 );
