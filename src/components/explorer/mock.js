5268a9cd6569a5e671b9d27853ae3957ef2de7c4
Fred Souza <fredericoamsouza@gmail.com>
2017-08-14 00:09:34 -0300

    Fixing tree styling

--- src/components/explorer/mock.js
+++ src/components/explorer/mock.js
@@ -30,7 +30,47 @@ export default {
         },
         'src': {
             'components': {
-                '_contents': []
+                '_contents': [],
+                'activityBar': { '_contents': [
+                    { name: 'index.js', path: 'src/components/activityBar/index.js' },
+                    { name: 'styles.css', path: 'src/components/activityBar/styles.css' }
+                ] },
+                'editor': { '_contents': [
+                    { name: 'index.js', path: 'src/components/editor/index.js' },
+                    { name: 'styles.css', path: 'src/components/editor/styles.css' }
+                ] },
+                'explorer': { '_contents': [
+                    { name: 'index.js', path: 'src/components/explorer/index.js' },
+                    { name: 'styles.css', path: 'src/components/explorer/styles.css' }
+                ] },
+                'fileTab': { '_contents': [
+                    { name: 'index.js', path: 'src/components/fileTab/index.js' },
+                    { name: 'styles.css', path: 'src/components/fileTab/styles.css' }
+                ] },
+                'fileTree': { '_contents': [
+                    { name: 'index.js', path: 'src/components/fileTree/index.js' },
+                    { name: 'styles.css', path: 'src/components/fileTree/styles.css' }
+                ] },
+                'header': { '_contents': [
+                    { name: 'index.js', path: 'src/components/header/index.js' },
+                    { name: 'styles.css', path: 'src/components/header/styles.css' }
+                ] },
+                'icon': { '_contents': [
+                    { name: 'index.js', path: 'src/components/icon/index.js' },
+                    { name: 'styles.css', path: 'src/components/icon/styles.css' }
+                ] },
+                'search': { '_contents': [
+                    { name: 'index.js', path: 'src/components/search/index.js' },
+                    { name: 'styles.css', path: 'src/components/search/styles.css' }
+                ] },
+                'sidebar': { '_contents': [
+                    { name: 'index.js', path: 'src/components/sidebar/index.js' },
+                    { name: 'styles.css', path: 'src/components/sidebar/styles.css' }
+                ] },
+                'textEditor': { '_contents': [
+                    { name: 'index.js', path: 'src/components/textEditor/index.js' },
+                    { name: 'styles.css', path: 'src/components/textEditor/styles.css' }
+                ] }
             },
             '_contents': [
                 {
