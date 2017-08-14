be06ed150cd4f634c6d1a7535a0cddb4e1eecda9
Fred Souza <fredericoamsouza@gmail.com>
2017-08-14 00:44:33 -0300

    Refactoring Search component title

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -6,7 +6,9 @@ export default class Search extends React.Component {
     render() {
         return (
             <div className="Search">
-                <h1>Search</h1>
+                <div className="title">
+                    <span>Search</span>
+                </div>
             </div>
         );
     }
