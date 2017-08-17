7dd17d1fa7707eb357bb20e715c093b2d121c548
Fred Souza <fredericoamsouza@gmail.com>
2017-08-17 15:18:35 -0300

    Receiving the project path by cli

--- server/config.js
+++ server/config.js
@@ -1,11 +1,12 @@
 'use strict';
+const resolve = require('path').resolve;
 /* global __dirname; */
 /**
  * Application configuration
  * You may use it to describe every global configuration data
  */
 module.exports = {
-    loadedDir: __dirname,
+    loadedDir: (process.argv[2]) ? resolve(process.argv[2]) : '',
     server: {
         ip: '0.0.0.0',
         port: 8080
