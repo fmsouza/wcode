0812047519aeb3216496188e8252dc016c3d3c4b
Fred Souza <fredericoamsouza@gmail.com>
2017-08-17 16:57:41 -0300

    Scrolling filetree content

--- src/components/fileTree/index.js
+++ src/components/fileTree/index.js
@@ -30,7 +30,7 @@ export default class FileTree extends React.Component {
         if (collapsed) return null;
         const { content: { files, folders } } = this.props;
         return (
-            <div className="subnodes no-padding">
+            <div className="rootContent subnodes no-padding">
                 {[].concat(folders.map(this.renderFolder)).concat(files.map(this.renderFile))}
             </div>
         );
