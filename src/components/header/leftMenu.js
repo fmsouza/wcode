d55f4b1a2bf7e1658c15d9a9f60e625b5173f0de
Fred Souza <fredericoamsouza@gmail.com>
2017-08-17 01:30:10 -0300

    Collapsing all header menus when clicking out of the menu

--- src/components/header/leftMenu.js
+++ src/components/header/leftMenu.js
@@ -5,22 +5,35 @@ export default class LeftMenu extends React.Component {
 
     state = { collapsed: { fileMenu: true } };
 
+    get menuCollapsed() {
+        return Object.keys(this.state.collapsed).map(key => this.state.collapsed[key])
+            .reduce((prev, next) => prev || next, false);
+    }
+
+    collapseAll = () => {
+        let { collapsed } = this.state;
+        collapsed = Object.keys(collapsed).reduce((prev, next) => ({ ...prev, [next]: true }), {});
+        this.setState({ collapsed });
+    }
+
+    toggle = (key) => this.setState({ collapsed: { [key]: !this.state.collapsed[key] }})
+
     callAndClose = (fn = () => {}) => () => {
-        this.setState({ collapsed: { fileMenu: !this.state.collapsed.fileMenu }});
+        this.collapseAll();
         fn();
     };
 
     renderFileMenu = (collapsed) => (
         <div className="menuItem">
-            <span className="title clickable" children="File" onClick={this.callAndClose()} />
+            <span className="title clickable" children="File" onClick={() => this.toggle('fileMenu')} />
             {!collapsed && (
                 <div className="submenu">
-                    <div className="clickable item" children="New file" onClick={this.callAndClose(Action.newFile)} />
-                    <div className="separator" />
-                    <div className="clickable item" children="Save" onClick={this.callAndClose(Action.saveFile)} />
-                    <div className="clickable item" children="Save all" onClick={this.callAndClose(Action.saveAllFiles)} />
-                    <div className="separator" />
-                    <div className="clickable item" children="Close file" onClick={this.callAndClose(Action.closeCurrentFile)} />
+                <div className="clickable item" children="New file" onClick={this.callAndClose(Action.newFile)} />
+                <div className="separator" />
+                <div className="clickable item" children="Save" onClick={this.callAndClose(Action.saveFile)} />
+                <div className="clickable item" children="Save all" onClick={this.callAndClose(Action.saveAllFiles)} />
+                <div className="separator" />
+                <div className="clickable item" children="Close file" onClick={this.callAndClose(Action.closeCurrentFile)} />
                 </div>
             )}
         </div>
@@ -31,6 +44,7 @@ export default class LeftMenu extends React.Component {
         return (
             <div className={`Menu ${className || ''}`}>
                 {this.renderFileMenu(this.state.collapsed.fileMenu)}
+                {!this.menuCollapsed && <div className="clickableOverlay" onClick={this.collapseAll} />}
             </div>
         );
     }
