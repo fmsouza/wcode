efa641ec9ae20c0d1bb0dfbd4202f3a94c7abbd1
Fred Souza <fredericoamsouza@gmail.com>
2017-08-19 11:34:50 -0300

    Closing without confirmation

--- src/common/services/control.js
+++ src/common/services/control.js
@@ -6,6 +6,6 @@ export const exit = () =>
     api.get('/control/exit')
     .then(({ status, data }) => {
         if (status === 200) {
-            window.close();
+            window.open('', '_self', ''); window.close();            
         }
     });
