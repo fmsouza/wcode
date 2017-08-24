a3abf4bd3cb26e526a33a3a04f177f83fec24c23
Fred Souza <fredericoamsouza@gmail.com>
2017-08-24 15:43:13 -0300

    Defining constants for available directory operations

--- server/constants.js
+++ server/constants.js
@@ -17,19 +17,25 @@ const SERVER_HOST = '0.0.0.0';
 const SERVER_PORT = 8080;
 
 const ActionTypes = {
-    READ_PROJECT: 'action/project/read',
-    CREATE_FILE: 'action/file/create',
-    READ_FILE: 'action/file/read',
-    UPDATE_FILE: 'action/file/update',
-    DELETE_FILE: 'action/file/delete',
-    EXIT_APP: 'action/control/exit',
-    KEEP_ALIVE: 'action/control/keepalive',
-    NOTIFICATION: 'action/control/notify'
+    READ_PROJECT:     'action/project/read',
+
+    CREATE_DIRECTORY: 'action/directory/create',
+    UPDATE_DIRECTORY: 'action/directory/update',
+    DELETE_DIRECTORY: 'action/directory/delete',
+
+    CREATE_FILE:      'action/file/create',
+    READ_FILE:        'action/file/read',
+    UPDATE_FILE:      'action/file/update',
+    DELETE_FILE:      'action/file/delete',
+
+    EXIT_APP:         'action/control/exit',
+    KEEP_ALIVE:       'action/control/keepalive',
+    NOTIFICATION:     'action/control/notify'
 };
 
 const NotificationTypes = {
-    INFO: 'notification/info',
-    ERROR: 'notification/error',
+    INFO:    'notification/info',
+    ERROR:   'notification/error',
     SUCCESS: 'notification/success',
     WARNING: 'notification/warning'
 };
