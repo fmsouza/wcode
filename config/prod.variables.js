00009f5566ef022facc7ccc2ff50b62a8beda6cb
Fred Souza <fredericoamsouza@gmail.com>
2017-08-26 13:31:19 -0300

    Automatically detects the local address when not in debug mode

--- config/prod.variables.js
+++ config/prod.variables.js
@@ -1,4 +1,3 @@
 export default {
-    'process.env.NODE_ENV': JSON.stringify('production'),
-    'process.env.API_URL': JSON.stringify('ws://localhost:9876')
+    'process.env.NODE_ENV': JSON.stringify('production')
 }
--- src/common/constants/general.js
+++ src/common/constants/general.js
@@ -1,11 +1,11 @@
+// {bool} DEBUG - Toggles debug mode on or off.
+export const DEBUG = process.env.NODE_ENV === "development";
+
 // {string} API_URL - Backend API HTTP address
-export const API_URL = process.env.API_URL;
+export const API_URL = (DEBUG) ? 'ws://localhost:9876' : `ws://${location.host}`;
 
 // {string} CONTACT_EMAIL - Support e-mail address
 export const CONTACT_EMAIL = "fredericoamsouza@gmail.com";
-
-// {bool} DEBUG - Toggles debug mode on or off.
-export const DEBUG = process.env.NODE_ENV === "development";
 
 // {string} NO_DATE - Moment js string for no date value set
 export const NO_DATE = '0001-01-01T00:00:00Z';
