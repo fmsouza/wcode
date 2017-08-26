0e08ef64a3f7fbb8cf89f937591a1c7416cb5d08
Fred Souza <fredericoamsouza@gmail.com>
2017-08-26 13:30:58 -0300

    Only kills the server when not in debug mode

--- src/setup.js
+++ src/setup.js
@@ -1,10 +1,11 @@
 import registerServiceWorker from './registerServiceWorker';
 import * as Stores from 'common/stores';
+import { General } from 'common/constants';
 import { registerKeyboardBindings } from 'common/utils';
 import { openConnection, Writer } from 'common/connection';
 
 window.addEventListener('resize', () => Stores.view.updateDimensions());
-window.addEventListener('unload', () => Writer.exit()); // When closing the view
+!General.DEBUG && window.addEventListener('unload', () => Writer.exit()); // When closing the view
 registerKeyboardBindings();
 registerServiceWorker();
 openConnection();
