ef99217c3b515fc315eb041f091ba0d9c039f10e
Fred Souza <fredericoamsouza@gmail.com>
2017-08-29 00:51:25 -0300

    Converting text read from keys

--- server/utils/security.js
+++ server/utils/security.js
@@ -6,8 +6,8 @@ const readSSLKeys = (keysPath) => {
     if (!stat.isDirectory()) throw new Error('The SSL keys path provided is not a directory.');
     try {
         return {
-            cert: fs.readFileSync(resolve(keysPath, 'certificate.pem')),
-            key: fs.readFileSync(resolve(keysPath, 'key.pem')),
+            cert: fs.readFileSync(resolve(keysPath, 'certificate.pem')).toString('utf-8'),
+            key: fs.readFileSync(resolve(keysPath, 'key.pem')).toString('utf-8'),
         };
     } catch (e) {
         throw new Error("Error: The SSL keys path must contain the 'certificate.pem' and the 'key.pem' files.");
