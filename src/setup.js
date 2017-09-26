eb4d6d8d40e5889d3415ad2197eeff2f5880ff9b
Fred Souza <fredericoamsouza@gmail.com>
2017-09-25 23:40:00 -0300

    Close window should not quit the server

--- src/setup.js
+++ src/setup.js
@@ -5,7 +5,6 @@ import { registerKeyboardBindings } from 'common/utils';
 import { openConnection, Writer } from 'common/connection';
 
 window.addEventListener('resize', () => Stores.view.updateDimensions());
-!General.DEBUG && window.addEventListener('unload', () => Writer.exit()); // When closing the view
 registerKeyboardBindings();
 registerServiceWorker();
 openConnection();
